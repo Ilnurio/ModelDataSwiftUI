d63e30161d17b765d7bff76616149d1402b25b57
Ilnur Fattakhov <nurich1995@mail.ru>
2023-06-14 20:41:08 +0300

    Initial Commit

---
+++ ModelDataSwiftUI/ModelDataSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelDataSwiftUIApp.swift
+//  ModelDataSwiftUI
+//
+//  Created by Ilnur on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ModelDataSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
